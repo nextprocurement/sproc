2a6b78d39b89b1cb489db75b375002e36d0d294f
Manuel A. Vázquez <manuavazquez@gmail.com>
2022-12-09 16:18:57 +0100

    initial commit (see 469b56448f5acb12848944c4945812a260fc4777 in )

---
+++ make-conda-environment.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+MANAGER="mamba"
+
+NAME=sproc
+
+LIBRARIES=(
+    jupyterlab
+    nbdev">"2.3
+    colorama
+    python-magic
+    pyyaml
+    html2text
+    lxml
+    pandas
+    pyarrow
+    openpyxl
+	ruamel.yaml
+)
+
+CHANNELS=(
+    defaults
+    conda-forge
+    fastai # nbdev
+)
+
+# ---
+
+COLOR="\033[40m\033[32m"
+UNCOLOR="\033[0m"
+
+
+# ------------ setup
+
+# only required if "anaconda" is not in the path
+source $HOME/$MY_CONDA_INSTALLATION/etc/profile.d/conda.sh
+
+# from https://stackoverflow.com/a/9429887/3967334
+LIBRARIES_CONCATENATED=$(IFS=" " ; echo "${LIBRARIES[*]}")
+
+# from https://stackoverflow.com/a/17841619/3967334
+function join_by { local d=${1-} f=${2-}; if shift 2; then printf %s "$f" "${@/#/$d}"; fi; }
+
+CHANNELS_CONCATENATED=$(join_by ' -c ' "${CHANNELS[@]}")
+
+# ------------ installation
+
+$MANAGER create --yes -n $NAME $LIBRARIES_CONCATENATED -c $CHANNELS_CONCATENATED
+
+# ------------ pip
+
+conda activate $NAME
+
+# pip stuff here....
+
+# ------------ nbdev
+
+# so that git is aware of nbdev/notebooks (only required once, when creating the repository, not through re-installs of conda)
+nbdev_install_hooks
+
+# this library is installed "live"
+pip install -e .
+
+# ------------
+
+echo -e new environment is \"$COLOR$NAME$UNCOLOR\"
+
+# # the environment is exported into a yaml file
+# conda env export --no-builds --from-history -f environment.yml
